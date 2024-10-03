8fb03139e603751881c417c7c0a6f0d2f743298c
Jordi Garreta <jordigarreta11@gmail.com>
2024-10-03 20:51:56 +0200

    improve body tag

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Header />
         <PageWrapper>{children}</PageWrapper>
         <Footer />
